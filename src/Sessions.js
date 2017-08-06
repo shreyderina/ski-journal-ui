7ff17c57f0eb4998f755d5e7691819ae90293d5b
ira <ira@debian>
2017-08-06 20:11:51 +0700

    Some changes in sessions

--- src/Sessions.js
+++ src/Sessions.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ExerciseList from './Exercise'
+import {Collapsible, CollapsibleItem} from 'react-materialize'
 
 class SessionList extends Component {
   constructor() {
@@ -17,28 +18,36 @@ class SessionList extends Component {
 
   render() {
     return (
-      <Table data={this.state.sessions} />
+      <Collapsible popout>
+
+        { this.state.sessions.map((session) =>
+          <CollapsibleItem header={session.time_of_day} icon='filter_drama'>
+            <ExerciseList id={session.id} />
+          </CollapsibleItem>
+        )}
+      </Collapsible>
     )
   }
 }
 
-let Table = (props) =>
-  <div className="row">
-    <div className="col s12">
-      <ul className="tabs">
-        {props.data.map((session) => <Tab data={session} />)}
-      </ul>
-    </div>
-    { props.data.map((session) => <TabContent data={session} />)}
-  </div>
 
-let Tab = (props) =>
-  <li className="tab col s3">
-    <a href={"#session" + props.data.id}>{props.data.id}</a></li>
-
-let TabContent = (props) =>
-  <div id={"session" + props.data.id} className="col s12">
-    <ExerciseList id={props.data.id} />
-  </div>
+// let Table = (props) =>
+//   <div className="row">
+//     <div className="col s12">
+//       <ul className="tabs">
+//         {props.data.map((session) => <Tab data={session} />)}
+//       </ul>
+//     </div>
+//     { props.data.map((session) => <TabContent data={session} />)}
+//   </div>
+//
+// let Tab = (props) =>
+//   <li className="tab col s3">
+//     <a href={"#session" + props.data.id}>{props.data.id}</a></li>
+//
+// let TabContent = (props) =>
+//   <div id={"session" + props.data.id} className="col s12">
+//     <ExerciseList id={props.data.id} />
+//   </div>
 
 export default SessionList;
